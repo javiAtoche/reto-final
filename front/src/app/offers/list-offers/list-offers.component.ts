de95ba5b1de5c7ee11d8214f0d66b0598a029eb3
icalo2046 <icalo_paro3@hotmail.comgit>
2019-06-07 12:41:05 +0200

    Updates listoffers offer y services
    Components borrados, creacion offer.service, offer.component, list-offers.component

--- front/src/app/offers/list-offers/list-offers.component.ts
+++ front/src/app/offers/list-offers/list-offers.component.ts
@@ -1,4 +1,7 @@
+import { OffersService } from './../offers.service';
 import { Component, OnInit } from '@angular/core';
+import { Offers } from '../model/offers';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-offers',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListOffersComponent implements OnInit {
 
-  constructor() { }
+  public offers: Array<Offers>;
+
+  constructor(private offerService: OffersService, private router: Router) { }
 
   ngOnInit() {
+    this.offerService.getOffers().subscribe(
+      data => { this.offers = data; });
   }
 
+  onSelect(offer: Offers) {
+    this.router.navigate(['/offers', offer.id]);
+  }
 }
