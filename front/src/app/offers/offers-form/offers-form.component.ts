4c24dd941dbea34b5689b56add6f53db63d0972d
unknown <ronsistemas>
2019-06-07 11:26:29 +0200

    base creada

---
+++ front/src/app/offers/offers-form/offers-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-form',
+  templateUrl: './offers-form.component.html',
+  styleUrls: ['./offers-form.component.css']
+})
+export class OffersFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
